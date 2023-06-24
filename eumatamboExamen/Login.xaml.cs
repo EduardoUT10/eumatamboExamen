f80f993b12a05e45d59e0e3ca8b37dcf3c13a7ec
Cotopaxi <Cotopaxi@LASPCSIS-01>
2023-06-23 19:52:31 -0500

    Agregar archivos de proyecto.

---
+++ eumatamboExamen/Login.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace eumatamboExamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+
+            string usuario = "estudiante2023";
+            string password = "uisrael2023";
+
+            if (txtUsuario.Text == usuario && txtPassword.Text == password)
+            {
+                Navigation.PushAsync(new Registro(usuario));
+            }
+            else
+            {
+                DisplayAlert("Error", "Usuario/Contraseña incorrectas", "Cerrar");
+            }
+        }
+    }
+}
---
+++ eumatamboExamen/Registro.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace eumatamboExamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {   string usuario;
+        public Registro(string usuario)
+        {
+            InitializeComponent();
+            lblUsuario.Text = "Usuario Conectado: " + usuario;
+            this.usuario = usuario;
+        }
+
+        private void btnPagoMensual_Clicked(object sender, EventArgs e)
+        {
+            double montoInicial = Convert.ToDouble(txtMontoInicial.Text);
+            double Mensual;
+            double fijo;
+            fijo = (1500 - montoInicial) / 4;
+
+            Mensual = fijo + (fijo * 0.04);
+            txtMontoMensual.Text = Mensual.ToString();
+        }
+
+        private void txtMontoInicial_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+              double dato = Convert.ToDouble(txtMontoInicial);
+              if (dato > 100 || dato < 1500)
+            {
+                 DisplayAlert("Error", "El monto debe ser mayor de 100 y menor de 1500", "Cerrar");
+                   txtMontoInicial.Text = "";
+               }
+            }
+            catch (Exception) { }
+        }
+    }
+    }
